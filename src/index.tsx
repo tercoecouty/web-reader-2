fcd378c531984f30656562604af9dfa154db6504
tercoecouty <fany12wen@gmail.com>
2022-06-06 11:39:44 +0800

    调整文件内容

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 // import { Provider } from "react-redux";
-import "./index.less";
+// import "./index.less";
 
 // import App from "./app/App";
 // import store from "./store";
