eaaec265dcd40e3e3f1d60f9eda7fd47ba73b94d
tercoecouty <fany12wen@gmail.com>
2022-06-06 17:58:19 +0800

    移除多余的文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
-// import "./index.less";
+import { Provider } from "react-redux";
+import "./index.less";
 
-// import App from "./app/App";
-// import store from "./store";
+import App from "./app/App";
+import store from "./store";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
-import Demo from "./Demo";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<Demo />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
