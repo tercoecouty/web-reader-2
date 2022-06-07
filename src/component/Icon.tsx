1224781eff1734f1e41d856efb2b24cd24dfa4c3
tercoecouty <fany12wen@gmail.com>
2022-06-07 19:05:11 +0800

    修复icon不会改变的问题

--- src/component/Icon.tsx
+++ src/component/Icon.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import "./Icon.less";
 
 interface IIconProps {
@@ -8,9 +8,10 @@ interface IIconProps {
 export default function Icon(props: IIconProps) {
     const ref = useRef(null);
 
+    // 这里每次都要执行，否则 svg 不会改变
     useEffect(() => {
         ref.current.innerHTML = props.svg;
-    }, []);
+    });
 
     return <span ref={ref} className="icon"></span>;
 }
