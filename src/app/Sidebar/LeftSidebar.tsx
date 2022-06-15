6a877b3391190334e6b45e0b68343e413ae2f143
tercoecouty <fany12wen@gmail.com>
2022-06-15 13:31:58 +0800

    查看别人批注的时候不可以查看自己的书签和笔记列表

--- src/app/Sidebar/LeftSidebar.tsx
+++ src/app/Sidebar/LeftSidebar.tsx
@@ -13,19 +13,37 @@ import Classes from "../Classes";
 import Notes from "../Notes";
 import Bookmarks from "../Bookmarks";
 
-import { selectShowClasses, selectShowBookmarks, appActions } from "../../slice/appSlice";
+import {
+    selectShowClasses,
+    selectShowBookmarks,
+    selectLoginUser,
+    selectNotesUser,
+    appActions,
+} from "../../slice/appSlice";
 
 export default function LeftSidebar() {
     const dispatch = useDispatch();
     const showClasses = useSelector(selectShowClasses);
     const showBookmarks = useSelector(selectShowBookmarks);
+    const loginUser = useSelector(selectLoginUser);
+    const notesUser = useSelector(selectNotesUser);
     const [showNotes, setShowNotes] = useState(false);
 
     return (
         <div style={{ width: "48px", borderRight: "1px solid black" }}>
             <SidebarItem svg={TeamSvg} title="班级列表" onClick={() => dispatch(appActions.setShowClasses(true))} />
-            <SidebarItem svg={BarsSvg} title="笔记" onClick={() => setShowNotes(true)} />
-            <SidebarItem svg={BookSvg} title="书签" onClick={() => dispatch(appActions.setShowBookmarks(true))} />
+            <SidebarItem
+                svg={BarsSvg}
+                title="笔记"
+                onClick={() => setShowNotes(true)}
+                disabled={notesUser.id !== loginUser.id}
+            />
+            <SidebarItem
+                svg={BookSvg}
+                title="书签"
+                onClick={() => dispatch(appActions.setShowBookmarks(true))}
+                disabled={notesUser.id !== loginUser.id}
+            />
             <SidebarItem svg={SearchSvg} title="搜索" disabled />
             <SidebarItem svg={SettingSvg} title="设置" disabled />
             <Drawer
