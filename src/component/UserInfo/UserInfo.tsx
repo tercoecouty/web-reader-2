a6374a4c1a302ec0e610f08bd16a2c9c3633d0f1
tercoecouty <fany12wen@gmail.com>
2022-06-16 15:01:01 +0800

    取消用户头像

--- src/component/UserInfo/UserInfo.tsx
+++ src/component/UserInfo/UserInfo.tsx
@@ -1,7 +1,4 @@
 import "./UserInfo.less";
-
-import DefaultAvatarImage from "./default-avatar.png";
-
 interface IUserInfoProps {
     name: string;
     dateTime: number;
@@ -22,7 +19,6 @@ const getDateTimeText = (timestamp) => {
 export default function UserInfo(props: IUserInfoProps) {
     return (
         <div className="user-info">
-            <img className="user-info-avatar" src={DefaultAvatarImage} />
             <div>
                 <div style={{ marginBottom: "4px" }}>{props.name}</div>
                 <div style={{ color: "gray", fontSize: "12px" }}>{getDateTimeText(props.dateTime)}</div>
