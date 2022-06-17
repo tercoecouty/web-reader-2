7770159321c8dbd7f2512e0c85051b80bfda638e
tercoecouty <fany12wen@gmail.com>
2022-06-17 12:13:02 +0800

    修改css变量名

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import Book from "./Book/Book";
 
 export default function App() {
     return (
-        <div className="app">
+        <div className="app theme-default">
             <LeftSidebar />
             <Book />
             <RightSidebar />
