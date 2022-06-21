c71c6f30663f81cdc5171807f941499f5d8f4ef6
tercoecouty <fany12wen@gmail.com>
2022-06-21 21:04:09 +0800

    分离NoteImages组件

--- src/app/Note/NoteEdit.tsx
+++ src/app/Note/NoteEdit.tsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
-import { createPortal } from "react-dom";
 import classNames from "classnames";
 import "./NoteEdit.less";
 
 import Icon from "../../component/Icon";
 import ArrowLeftSvg from "../../svg/arrow-left.svg?raw";
 import SendSvg from "../../svg/send.svg?raw";
-import DeleteSvg from "../../svg/delete.svg?raw";
-import EyeSvg from "../../svg/eye.svg?raw";
+
+import NoteImages from "./NoteImages";
 
 interface INoteEditProps {
     initialText?: string;
@@ -23,8 +22,6 @@ export default function NoteEdit(props: INoteEditProps) {
     const [value, setValue] = useState(initialText || "");
     const [fileMap, setFileMap] = useState<Map<string, File>>(new Map());
     const [hasChange, setHasChange] = useState(false);
-    const [previewUrl, setPreviewUrl] = useState("");
-    const [showPreview, setShowPreview] = useState(false);
 
     useEffect(() => {
         setShow(true);
@@ -50,8 +47,7 @@ export default function NoteEdit(props: INoteEditProps) {
         setHasChange(true);
     };
 
-    const handleFileChange = (e) => {
-        const file = e.target.files[0] as File;
+    const handleUpload = (file: File) => {
         const url = URL.createObjectURL(file);
         fileMap.set(url, file);
         setFileMap(new Map(fileMap));
@@ -72,61 +68,6 @@ export default function NoteEdit(props: INoteEditProps) {
         setTimeout(() => onClose(), 300);
     };
 
-    const handleShowPreview = (url: string) => {
-        setPreviewUrl(url);
-        setTimeout(() => setShowPreview(true), 0);
-    };
-
-    const handleHidePreview = () => {
-        setShowPreview(false);
-        setTimeout(() => setPreviewUrl(""), 300);
-    };
-
-    const renderImages = () => {
-        const domImages = [];
-        for (const url of fileMap.keys()) {
-            domImages.push(
-                <div className="image-item" key={url}>
-                    <img src={url} />
-                    <div className="image-item-hover">
-                        <Icon svg={EyeSvg} onClick={() => handleShowPreview(url)} />
-                        <Icon svg={DeleteSvg} onClick={() => deleteImage(url)} />
-                    </div>
-                </div>
-            );
-        }
-
-        if (domImages.length < 9) {
-            domImages.push(
-                <div
-                    className="image-upload"
-                    onClick={() => document.getElementById("image-file-input").click()}
-                    key="image-upload"
-                >
-                    <span>
-                        <span></span>
-                        <span></span>
-                    </span>
-                </div>
-            );
-        }
-
-        return domImages;
-    };
-
-    const renderPreview = () => {
-        return createPortal(
-            <div className="image-preview">
-                <div
-                    className={classNames("image-preview-background", { show: showPreview })}
-                    onClick={handleHidePreview}
-                ></div>
-                <img className={classNames({ show: showPreview })} src={previewUrl} />
-            </div>,
-            document.body
-        );
-    };
-
     return (
         <div className={classNames("note-edit", { show })}>
             <div className="note-edit-header">
@@ -139,17 +80,7 @@ export default function NoteEdit(props: INoteEditProps) {
                 onInput={handleChange}
                 placeholder="在这里输入……"
             ></textarea>
-            <div className="note-edit-images">
-                <input
-                    className="image-file-input"
-                    id="image-file-input"
-                    type="file"
-                    accept=".jpg,.png"
-                    onInput={handleFileChange}
-                />
-                {renderImages()}
-                {previewUrl && renderPreview()}
-            </div>
+            <NoteImages urls={[...fileMap.keys()]} showUpload={true} onDelete={deleteImage} onUpload={handleUpload} />
             <div className="note-edit-submit">
                 <span className="letter-count">{value.length} / 200</span>
                 <Icon svg={SendSvg} onClick={submit} className={classNames({ disabled: !hasChange })} />
---
+++ src/app/Note/NoteImages.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import classNames from "classnames";
+import "./NoteImages.less";
+
+import Icon from "../../component/Icon";
+import DeleteSvg from "../../svg/delete.svg?raw";
+import EyeSvg from "../../svg/eye.svg?raw";
+
+interface INoteImagesProps {
+    urls: string[];
+    showUpload?: boolean;
+    onUpload: (file: File) => void;
+    onDelete: (url: string) => void;
+}
+
+export default function NoteImages(props: INoteImagesProps) {
+    const { urls, onDelete, onUpload } = props;
+    const showUpload = props.showUpload || false;
+    const [previewUrl, setPreviewUrl] = useState("");
+    const [showPreview, setShowPreview] = useState(false);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0] as File;
+        onUpload(file);
+    };
+
+    const handleShowPreview = (url: string) => {
+        setPreviewUrl(url);
+        setTimeout(() => setShowPreview(true), 0);
+    };
+
+    const handleHidePreview = () => {
+        setShowPreview(false);
+        setTimeout(() => setPreviewUrl(""), 300);
+    };
+
+    const renderImages = () => {
+        const domImages = [];
+        for (const url of urls) {
+            domImages.push(
+                <div className="image-item" key={url}>
+                    <img src={url} />
+                    <div className="image-item-hover">
+                        <Icon svg={EyeSvg} onClick={() => handleShowPreview(url)} />
+                        <Icon svg={DeleteSvg} onClick={() => onDelete(url)} />
+                    </div>
+                </div>
+            );
+        }
+
+        if (showUpload && domImages.length < 9) {
+            domImages.push(
+                <div
+                    className="image-upload"
+                    onClick={() => document.getElementById("image-file-input").click()}
+                    key="image-upload"
+                >
+                    <span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                </div>
+            );
+        }
+
+        return domImages;
+    };
+
+    const renderPreview = () => {
+        return createPortal(
+            <div className="image-preview">
+                <div
+                    className={classNames("image-preview-background", { show: showPreview })}
+                    onClick={handleHidePreview}
+                ></div>
+                <img className={classNames({ show: showPreview })} src={previewUrl} />
+            </div>,
+            document.body
+        );
+    };
+
+    return (
+        <div className="note-edit-images">
+            <input
+                className="image-file-input"
+                id="image-file-input"
+                type="file"
+                accept=".jpg,.png"
+                onInput={handleFileChange}
+            />
+            {renderImages()}
+            {previewUrl && renderPreview()}
+        </div>
+    );
+}
