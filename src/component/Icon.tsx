c6f5b6b2475349d3dadc0a03edddde864fbf8f41
tercoecouty <fany12wen@gmail.com>
2022-07-21 19:57:54 +0800

    调整icon组件

--- src/component/Icon.tsx
+++ src/component/Icon.tsx
@@ -4,16 +4,14 @@ import "./Icon.less";
 
 interface IIconProps {
     svg: string;
-    className?: string;
     onClick?: () => void;
     disabled?: boolean;
 }
 
 export default function Icon(props: IIconProps) {
     const ref = useRef(null);
-    const { className, svg, onClick, disabled } = props;
+    const { svg, onClick, disabled } = props;
 
-    // 这里每次都要执行，否则 svg 不会改变
     useEffect(() => {
         ref.current.innerHTML = svg;
     }, [svg]);
@@ -22,5 +20,5 @@ export default function Icon(props: IIconProps) {
         if (!disabled && onClick) onClick();
     };
 
-    return <span onClick={handleClick} ref={ref} className={classNames("icon", className, { disabled })}></span>;
+    return <span onClick={handleClick} ref={ref} className={classNames("icon", { disabled })}></span>;
 }
